8977083521d5c2bdc35d322d2d7de609e8b24ce1
Ryan Richards <ryanjr@me.com>
2021-08-23 13:24:37 +0100

    scroll

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,10 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes,
+    scrollBehavior (to, from, savedPosition){
+    return {x:0,y:0}
+    }
 })
 
 export default router
