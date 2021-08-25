4142c4f35d61d9230c143057677e5e3ec35faf4e
Ryan Richards <ryanjr@me.com>
2021-08-25 09:14:53 +0100

    scroll test mobile

--- src/router/index.js
+++ src/router/index.js
@@ -33,14 +33,12 @@ const router = createRouter({
     routes,
     scrollBehavior (to, from, savedPosition) {
         if (to.hash) {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  resolve({
+            return  {
+   
                       el: to.hash,
                       behavior: 'smooth',
-                    })
-                }, 500)
-              })
+                    }
+              
             } else {
           return {left: 0, top: 0, behavior: 'smooth'}
         }
