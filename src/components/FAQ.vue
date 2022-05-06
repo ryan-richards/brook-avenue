b8be8818acaee0085ad245cb314920e1e8a3a7c2
ryan <ryan.richards@phasora.net>
2022-05-06 14:08:14 +0100

    Alt Tags Added

---
+++ src/components/FAQ.vue
@@ -0,0 +1,55 @@
+<template>
+
+<section class="section  is-clipped">
+  <div class="container">
+
+    <div  style="display:flex; flex-direction:column; gap:20px; text-align:center;"  >
+    
+    <h1 class="question">What is the difference between Gelato and Ice Cream?</h1>
+    <p>Gelato and Ice cream are very similar...</p>
+
+    <h2 class="question">Why book an Ice Cream cart instead of an Ice Cream van?</h2>
+
+    <h2 class="question">Where do you get your ice cream from?</h2>
+    
+    <h2 class="question">How do I check availability?</h2>
+    
+    <h2 class="question">Do I pay a deposit to secure my booking?</h2>
+    
+    <h2 class="question">How early should I book?</h2>
+    
+    <h2 class="question">When do I finalise my details</h2>
+    
+    <h2 class="question">When will you arrive at my event?</h2>
+    
+    <h1 class="question">How long do you stay at a wedding?</h1>
+
+    <h2 class="question">How many flavours can I choose?</h2>
+    
+    <h2 class="question">How do I choose my flavours?</h2>
+    
+    <h2 class="question">Do you provide samples?</h2>
+    
+    <h2 class="question">Can I personalise wafers or tubs?</h2>
+    
+    <h2 class="question">What if it Rains?</h2>
+
+    <h2 class="question">What if I need to cancel or change my booking?</h2>
+    </div>
+
+  </div>
+</section>
+
+
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+.question {
+    font-weight: 900;
+}
+
+</style>
