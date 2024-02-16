1102aaaca245228cbf000305ebe13dc6576bf5ae
Ryan Richards <ryanrichardsdev@gmail.com>
2024-02-16 15:07:35 +0000

    add logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,107 +1,115 @@
 <template>
+  <div class="container">
+    <nav class="navbar pb-2 mobilePadding">
+      <div class="navbar-brand">
+        <router-link to="/">
+          <a @click="close" class="pr-2 pl-2 mr-6">
+            <img
+              src="../assets/brook-logo.png"
+              style="max-height: 4.5vh"
+              alt="Brook Avenue, Artisan Gelato, Belfast Northern Ireland Logo"
+            />
+          </a>
+        </router-link>
 
-<div class="container">
+        <a
+          role="button"
+          @click="toggle"
+          v-bind:class="{ 'is-active': isActive, 'toggle-off': !isActive }"
+          class="navbar-burger mr-0"
+          aria-label="menu"
+          aria-expanded="false"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
-<nav class="navbar pb-2 mobilePadding">
-   <div class="navbar-brand">
-       <router-link to="/">
-        <a @click="close" class="navbar-item mr-6 has-text-left">
-            <div>
-            <h1 style="font-weight:800;">Brook Avenue</h1>
-            <h2>Artisan Gelato</h2>
-            </div>
-            </a>
-       </router-link>
-    
-            
-        <a role="button" @click="toggle" v-bind:class="{'is-active': isActive, 'toggle-off': !isActive}" class="navbar-burger mr-0" aria-label="menu" aria-expanded="false">
-         <span aria-hidden="true"></span>
-         <span aria-hidden="true"></span>
-         <span aria-hidden="true"></span>
-         </a>
-        </div>
-            
-     <div class="navbar-menu" v-bind:class="{'is-active': isActive, 'toggle-off': !isActive}" >
+      <div
+        class="navbar-menu"
+        v-bind:class="{ 'is-active': isActive, 'toggle-off': !isActive }"
+      >
         <div class="navbar-end">
-        <router-link to="/"><a @click="toggle" class="navbar-item list pt-4">Home</a></router-link>
-        <router-link to="/pricing"><a  @click="toggle" class="navbar-item list pt-4">Pricing</a></router-link>
-        <router-link to="/about"><a    @click="toggle" class="navbar-item list pt-4">About</a></router-link>
-        <router-link to="/contact"><a  @click="toggle" class="navbar-item list pt-4">Contact</a></router-link>
-            </div>
-         </div>
-</nav>
-
-</div>
-
-
-
-
-
-  
+          <router-link to="/"
+            ><a @click="toggle" class="navbar-item list pt-4"
+              >Home</a
+            ></router-link
+          >
+          <router-link to="/pricing"
+            ><a @click="toggle" class="navbar-item list pt-4"
+              >Pricing</a
+            ></router-link
+          >
+          <router-link to="/about"
+            ><a @click="toggle" class="navbar-item list pt-4"
+              >About</a
+            ></router-link
+          >
+          <router-link to="/contact"
+            ><a @click="toggle" class="navbar-item list pt-4"
+              >Contact</a
+            ></router-link
+          >
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
-    data () {
-        return {
-            isActive: false
-        }
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.isActive = !this.isActive;
     },
-    methods: {
-        toggle(){
-         this.isActive = !this.isActive
-        },
-        close() {
-            if (this.isActive){
-            this.isActive = !this.isActive
-            } 
-        }
+    close() {
+      if (this.isActive) {
+        this.isActive = !this.isActive;
+      }
     },
-}
-
-
+  },
+};
 </script>
 
 <style scoped>
-
-a.navbar-item:hover{
-    color: tomato;
-    background-color: transparent;
+a.navbar-item:hover {
+  color: tomato;
+  background-color: transparent;
 }
 
-.navbar-menu{
-    box-shadow: none;
-    background-color:transparent;
+.navbar-menu {
+  box-shadow: none;
+  background-color: transparent;
 }
 
-.navbar-menu.is-active{
-    box-shadow: none;
-    background-color:white;
+.navbar-menu.is-active {
+  box-shadow: none;
+  background-color: white;
 }
 
-.navbar{
-    background-color: transparent;
+.navbar {
+  background-color: transparent;
 }
-
-
-
 
 .container {
-    margin-top: -1rem;
+  margin-top: -1rem;
 }
 
 @media (max-width: 1024px) {
-	.mobilePadding{
-
-		margin-left: -0.6rem;
-	}
+  .mobilePadding {
+    margin-left: -0.6rem;
+  }
 }
-
 
 @media screen and (min-width: 1024px) {
-	.mobilePadding{
-		padding-right: 1.8rem;
-	}
+  .mobilePadding {
+    padding-right: 1.8rem;
+  }
 }
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,48 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/about',
-        name: 'About',
-        component: () => import("../views/About.vue")
-    },
-    {
-        path: '/pricing',
-        name: 'Pricing',
-        component: () => import("../views/Pricing.vue")
-    },
-    {
-        path: '/contact',
-        name: 'Contact',
-        component: () => import("../views/Contact.vue")
-    },
-    { path: '/post/:id', 
-      component: () => import("../views/Post.vue")
-    },
-
-]
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue"),
+  },
+  {
+    path: "/pricing",
+    name: "Pricing",
+    component: () => import("../views/Pricing.vue"),
+  },
+  {
+    path: "/blog",
+    name: "Blog",
+    component: () => import("../views/Blog.vue"),
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: () => import("../views/Contact.vue"),
+  },
+  { path: "/post/:id", component: () => import("../views/Post.vue") },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-    scrollBehavior (to, from, savedPosition) {
-        if (to.hash) {
-            return  {
-   
-                      el: to.hash,
-                      behavior: 'smooth',
-                    }
-              
-            } else {
-          return {left: 0, top: 0, behavior: 'smooth'}
-        }
-    
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: "smooth",
+      };
+    } else {
+      return { left: 0, top: 0, behavior: "smooth" };
     }
-})
+  },
+});
 
-export default router
-
+export default router;
